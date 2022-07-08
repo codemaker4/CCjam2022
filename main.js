1af64cc307873d4c45718534d47720378bb180b7
CodeMaker4 <codemaker04@gmail.com>
2022-07-08 11:29:28 +0200

    basic player platform works

--- main.js
+++ main.js
@@ -9,9 +9,13 @@ function setup() {
     world = new World();
 
     world.platforms = [
-        new Platform(createVector(width/2, height), createVector(width*0.9, 20))
+        new Platform(createVector(world.size.x/2, world.size.y), createVector(world.size.x*0.9, 20))
     ];
 }
+
+setTimeout(() => {
+    world.players.push(new Player(world, thisPlayerName, createVector(world.size.x/2, 200), createVector(0,0), color('blue')));
+}, 2000);
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
@@ -21,6 +25,10 @@ function windowResized() {
 function draw() {
     background(255);
     scale(0.5);
+    fill(0,0,0,0);
+    stroke(0);
+    strokeWeight(2);
     rect(0, 0, world.size.x, world.size.y);
+    world.tick();
     world.draw();
   }
