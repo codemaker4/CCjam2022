33c86f96b2f3cfa02b8a628ed26fba536721946b
Jochem Baas <jochembaas3@gmail.com>
2022-07-08 14:07:50 +0200

    Update socket.js

--- socket.js
+++ socket.js
@@ -26,8 +26,17 @@ socket.on("playerUpdate", (gameData) => {
 
 function updatePlayers(gameData){
     gameData.forEach(player => {
-        console.log(player)
+        player = JSON.parse(player)
+        playerObj = world.getPlayer(player["name"])
+
+        console.log(player.pos, playerObj.pos)
+        world.getPlayer(player["name"])["pos"].set(player["pos"])
+        // world.getPlayer(player["name"])["vel"] = player["vel"]
+        // world.getPlayer(player["name"])["size"] = player["size"]
+        // world.getPlayer(player["name"])["color"] = player["color"]
+        // world.getPlayer(player["name"])["holdButtons"] = player["holdButtons"]
+        // world.getPlayer(player["name"])["pressButtons"] = player["pressButtons"]
+        // world.getPlayer(player["name"])["framesSinceOnGround"] = player["framesSinceOnGround"]
     })
 
-    world.getPlayer(gameData)
 }
